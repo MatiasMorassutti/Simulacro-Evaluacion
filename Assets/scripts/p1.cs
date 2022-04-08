fe40305bcb0bfab8a96e1df614bd1717a2ef5531
MatiasMorassutti <101564189+MatiasMorassutti@users.noreply.github.com>
2022-04-08 11:47:29 -0300

    Initial commit

---
+++ Assets/scripts/p1.cs
@@ -0,0 +1,28 @@
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p1 : MonoBehaviour
+{
+    public int valor1;
+    public int valor2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (valor1 == valor2)
+        {
+            Debug.Log("Los dos valores son iguales");
+        }
+        else 
+        {
+            Debug.Log("Los dos valores no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/p2.cs
@@ -0,0 +1,32 @@
+﻿//2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float producto1;
+    public float producto2;
+    public float producto3;
+    public float montodisponible1;
+    void Start()
+    {
+        if (montodisponible1 < producto1 + producto2 + producto3)
+        {
+            Debug.Log("La suma de dinero supera al monto disponible");
+        }
+
+        else if (montodisponible1 > producto1 + producto2 + producto3)
+        {
+            Debug.Log("La suma de dinero no supera al monto disponible");
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
